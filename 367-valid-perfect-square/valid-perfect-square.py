09f06770bac109e582abff9f2695f84235306d41
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2024-07-27 19:37:43 +0530

    Time: 32 ms (78.67%) | Memory: 16.5 MB (16.00%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,4 +1,15 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        return sqrt(num) == floor(sqrt(num))
-            
+        if num < 0:
+            return False
+        low,high = 0,num
+        while low <= high:
+            mid = (low + high) // 2
+            sqr = mid * mid
+            if sqr == num:
+                return True
+            elif sqr < num:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
