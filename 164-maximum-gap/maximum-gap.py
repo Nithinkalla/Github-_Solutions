4fcfd8a8a67964673e199ea2416696f337e4cc45
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2024-08-02 19:26:13 +0530

    Time: 726 ms (87.67%) | Memory: 30.5 MB (53.43%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -3,6 +3,5 @@ class Solution:
         if len(nums) < 2:
             return 0
         nums.sort()
-        x =[]
         diff = [nums[i + 1] - nums[i] for i in range(len(nums) - 1)]
         return max(diff)
