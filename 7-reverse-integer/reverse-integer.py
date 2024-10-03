a443c8e28ec0d4ef2861807764cf58536d9d9983
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2024-10-03 08:20:16 +0530

    Time: 44 ms (9.56%) | Memory: 16.4 MB (82.43%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,9 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
         y = str(x)
-        z = list(y)
-        jnstr = "".join(z)
-        rev = jnstr[::-1]
+        rev = y[::-1]
         if x < 0:
             rev = "-" + rev[:-1]
         if int(rev) > 2 **31 - 1 or int(rev) < -2**31:
