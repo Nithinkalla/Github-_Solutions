9198b267c79f8cb4e1ce4757187de28bd346f69c
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2025-01-06 12:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (15.35%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,11 @@
+from math import comb
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return 1
-        prev ,curr = 1,1
-        for i in range(2,n + 1):
-            temp = curr
-            curr = prev + curr
-            prev = temp
-        return curr
+        total_ways = 0
+        # Loop through possible counts of 2-steps
+        for two_steps in range(n // 2 + 1):
+            one_steps = n - 2 * two_steps
+            # Calculate permutations using combinations formula
+            total_ways += comb(one_steps + two_steps, two_steps)
+        return total_ways
