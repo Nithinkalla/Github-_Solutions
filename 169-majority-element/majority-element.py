441c1f3d453874b8eaf8a3bb7af9d6019fce6550
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2025-01-28 21:36:01 +0530

    Time: 7 ms (57.80%) | Memory: 19.5 MB (14.64%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,3 +1,9 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return statistics.mode(nums)
+        x = Counter(nums)
+        y = x.values()
+        z = max(y)
+        res = [k for k,v in x.items() if v == z]
+        for i in res:
+            return i
