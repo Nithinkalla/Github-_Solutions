d0ed54a6d754e56f436260253915869c51400c97
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2025-01-31 16:47:40 +0530

    Time: 9 ms (75.69%) | Memory: 18.5 MB (20.74%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,14 +1,12 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        res = []
-        carry = k
+        x = []
+        c = k
         i = len(num) - 1
-
-        while i >= 0 or carry > 0:
+        while i >= 0 or c > 0:
             if i >= 0:
-                carry += num[i]  # Add current digit
+                c += num[i]
                 i -= 1
-            res.append(carry % 10)  # Extract last digit
-            carry //= 10  # Carry for next digit
-
-        return res[::-1]  # Reverse to get the correct order
+            x.append(c % 10)
+            c //= 10
+        return x[::-1]
