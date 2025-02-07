e54abe31b51758e41e6f98ea12e38d229bb27b6e
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2025-02-07 17:55:46 +0530

    Time: 7 ms (34.72%) | Memory: 19.9 MB (8.57%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,7 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x = 0
-        for i in nums:
-            x = x ^ i
-        return x
+        x = Counter(nums)
+        for k,v in x.items():
+            if v == 1:
+                return k
