5c28fafcf98f18a263d3646bf97aaa8e227473df
nithinkalla <98216821+Nithinkalla@users.noreply.github.com>
2025-10-12 19:20:37 +0530

    Time: 5 ms (4.67%) | Memory: 18.1 MB (14.68%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,5 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while '()' in s or '[]' in s or '{}' in s:
-            s = s.replace("()","").replace("[]","").replace("{}","")
-        return not s
+        mapping = {')':'(','}':'{',']':'['}
+        lst = []
+        for i in s:
+            if i in mapping.values():
+                lst.append(i)
+            elif i in mapping:
+                if not lst or lst[-1] not in mapping[i]:
+                    return False
+                lst.pop()
+        return not lst
+        # while '()' in s or '[]' in s or '{}' in s:
+        #     s = s.replace("()","").replace("[]","").replace("{}","")
+        # return not s
